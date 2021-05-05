a029392f74944e9c3dfaf8230bf9e78cc4ac5e43
Fatemeh <rahmanzadeh8922@yahoo.com>
2021-05-05 16:21:40 +0430

    some attributes to work and user objects, new methods

--- main.py
+++ main.py
@@ -6,14 +6,12 @@ while True:
     action = input('you should have an account to enter:'
                    '\n  1. I have an account.    2. I am new to reminder   3. exit reminder \n\n'
                    'enter a number from menu above -->>>:')
-    try:
-        if action == '1':
-            menu_manager.login()
-        elif action == '2':
-            menu_manager.creat_account()
-        elif action == '3':
-            break
-        else:
-            raise ValueError
-    except ValueError:
+
+    if action == '1':
+        menu_manager.login()
+    elif action == '2':
+        menu_manager.creat_account()
+    elif action == '3':
+        break
+    else:
         print('invalid choice please try again')
